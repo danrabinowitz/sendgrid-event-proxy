3a3820e5027e678f67ecad3e8f7108cb8b165f12
Dan Rabinowitz <djr@danielrabinowitz.com>
2012-03-13 12:09:02 -0600

    Fix error with client and category not being defined properly

--- app/mailers/warning_mailer.rb
+++ app/mailers/warning_mailer.rb
@@ -2,6 +2,9 @@ class WarningMailer < ActionMailer::Base
   default from: "sendgrid-event-proxy@vanheyst.com"
 
   def unknown_client_email(category,client)
+    @category = category
+    @client = client
+    headers["category"] = "sendgrid-event-proxy#unknown_client_email"
     mail(:to => 'dan@vanheyst.com', :subject => "WARNING: Invalid Sendgrid category")
   end
 end
--- app/models/sendgrid_event.rb
+++ app/models/sendgrid_event.rb
@@ -30,14 +30,24 @@ class SendgridEvent < ActiveRecord::Base
     return nil if category.nil?
     client,* = category.split('#') # "client1#campaign1#a"
     return nil if client.nil? or client.downcase=='test'
+    return nil if ["sendgrid-event-proxy"].includes? client.downcase  # This avoids a circular reference, where warning emails generate warning emails!
     url = SendgridEventProxy::Application.config.destination_urls[client.downcase]
-    WarningMailer.unknown_client_email(category,client).deliver if url.nil?
+    begin
+      WarningMailer.unknown_client_email(category,client).deliver if url.nil?
+    rescue
+      logger.warn("Unable to send email: #{$!}")
+      raise
+    end
+    logger.debug("x1b")
     return url
   end
   
   def post
+    logger.debug("x1")
     return true if self.url_to_post.nil?
+    logger.debug("x2")
     begin
+      logger.debug("x3")
       Curl::Easy::http_post(self.url_to_post,self.to_ampersand_separated_s)
     rescue
       logger.warn("Unable to post to #{self.url_to_post}: #{$!}")
